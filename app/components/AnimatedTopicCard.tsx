47922f40814fd592fd64d1d43401ba6f24e8be2e
Dyfintie <v08pandey@gmail.com>
2025-05-28 01:01:31 +0530

    update

--- app/components/AnimatedTopicCard.tsx
+++ app/components/AnimatedTopicCard.tsx
@@ -63,11 +63,14 @@ export default function AnimatedTopicCard({ topic, onHome }) {
         />
         <span className="font-semibold text-black">{topic.seller}</span>
         {onHome && (
-          <div>
+          <div className="flex gap-2">
             <RemoveBtn id={topic._id} />
 
             <Link href={`/editTopic/${topic._id}`}>
-              <button className="btn rounded-md" title="Edit">
+              <button
+                className="px-5 py-1  border-2 border-black dark:border-white uppercase bg-white text-black transition duration-200 text-sm shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] rounded-md"
+                title="Edit"
+              >
                 <EditIcon />
               </button>
             </Link>
--- app/components/RemoveBtn.tsx
+++ app/components/RemoveBtn.tsx
@@ -21,7 +21,10 @@ export default function RemoveBtn({ id }) {
   };
 
   return (
-    <button onClick={removeTopic} className="btn  rounded-md text-red-400">
+    <button
+      onClick={removeTopic}
+      className="px-5 py-1 border-2 border-black dark:border-white uppercase bg-white  transition duration-200 text-sm shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] rounded-md text-red-600"
+    >
       <DeleteIcon />
     </button>
   );
